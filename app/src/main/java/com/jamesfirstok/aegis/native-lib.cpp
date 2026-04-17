e084d09e42a2767604d2c309259042c641f08ca2
Ali Alamari <alialamari772789210@gmail.com>
2026-04-17 22:34:49 +0300

    native-lib.cpp
    Signed-off-by: Ali Alamari <alialamari772789210@gmail.com>

--- app/src/main/java/com/jamesfirstok/aegis/native-lib.cpp
+++ app/src/main/java/com/jamesfirstok/aegis/native-lib.cpp
@@ -2,11 +2,17 @@
 #include <string>
 
 extern "C" JNIEXPORT jstring JNICALL
-Java_com_jamesfirstok_aegis_MainActivity_validateSecurity(
+/**
+ * تم تعديل اسم الدالة ليتوافق مع المسار الجديد:
+ * Java_ [اسم الحزمة مع استبدال النقاط بشرطات سفلية] _ [اسم الملف] _ [اسم الدالة]
+ */
+Java_com_aegis_tactical_v108_MainActivity_validateSecurity(
         JNIEnv* env,
         jobject /* this */) {
     
     // مفتاح التشفير الخاص للارتباط بالقمر الصناعي AEGIS-992-DELTA
-    std::string securityKey = "AEGIS-992-DELTA-AUTHENTICATED";
+    // تم تأمين المفتاح ليكون مرتبطاً باسم العقيد علي العماري
+    std::string securityKey = "AEGIS-992-DELTA-AUTHENTICATED-AL-AMMARI";
+    
     return env->NewStringUTF(securityKey.c_str());
 }
